bc61d8300014b03649a2e8125bd6f42c324b322e
onqtam <vik.kirilov@gmail.com>
2017-05-28 16:12:30 +0300

    removed all trace of glew - wont be needing it when using bgfx also moved some cmake code around

--- scripts/python/setup.py
+++ scripts/python/setup.py
@@ -58,9 +58,3 @@ if not os.path.exists('third_party/boost_1_' + boost_minor + '_0'):
     url = 'http://sourceforge.net/projects/boost/files/boost/1.' + boost_minor + '.0/boost_1_' + boost_minor + '_0.zip/download'
     downloadAndExtractZip(url, "third_party/boost.zip", "third_party")
     os.remove("third_party/boost.zip")
-
-glew_ver = "2.0.0"
-if not os.path.exists("third_party/glew-" + glew_ver):
-    url = 'https://sourceforge.net/projects/glew/files/glew/2.0.0/glew-' + glew_ver + '.zip/download'
-    downloadAndExtractZip(url, "third_party/glew.zip", "third_party")
-    os.remove("third_party/glew.zip")
