dddb81d2041fad65d77b2197a5fa829bf1f0510d
onqtam <vik.kirilov@gmail.com>
2017-06-16 12:35:50 +0300

    temp commit

--- src/core/Application.cpp
+++ src/core/Application.cpp
@@ -1,12 +1,7 @@
 #include "Application.h"
 #include "PluginManager.h"
+#include "utils/utils.h"
 
-#ifdef _WIN32
-#define GLFW_EXPOSE_NATIVE_WIN32
-#endif
-#ifdef __APPLE__
-#define GLFW_EXPOSE_NATIVE_COCOA
-#endif
 #include <bx/fpumath.h>
 #include <bgfx/platform.h>
 #include <bgfx/bgfx.h>
@@ -14,52 +9,175 @@
 #ifdef EMSCRIPTEN
 #include <emscripten/emscripten.h>
 #else // EMSCRIPTEN
+#ifdef _WIN32
+#define GLFW_EXPOSE_NATIVE_WIN32
+#endif // _WIN32
+#ifdef __APPLE__
+#define GLFW_EXPOSE_NATIVE_COCOA
+#endif // __APPLE__
 #include <GLFW/glfw3native.h>
 #endif // EMSCRIPTEN
 
 #include <imgui.h>
-
 #include <fstream>
 
-// bgfx utils
 const bgfx::Memory* loadMemory(const char* filename);
 bgfx::ShaderHandle loadShader(const char* shader);
 bgfx::ProgramHandle loadProgram(const char* vsName, const char* fsName);
 
-#include "bigg_imgui.hpp"
+// =================================================================================================
+// == IMGUI ========================================================================================
+// =================================================================================================
 
-#include "utils/utils.h"
-// bgfx utils
+static bgfx::VertexDecl    imguiVertexDecl;
+static bgfx::TextureHandle imguiFontTexture;
+static bgfx::UniformHandle imguiFontUniform;
+static bgfx::ProgramHandle imguiProgram;
+static void imguiRender(ImDrawData* drawData);
+static void        imguiShutdown();
+static const char* imguiGetClipboardText(void* userData);
+static void imguiSetClipboardText(void* userData, const char* text);
 
-const bgfx::Memory* loadMemory(const char* filename) {
-    std::ifstream   file(filename, std::ios::binary | std::ios::ate);
-    std::streamsize size = file.tellg();
-    file.seekg(0, std::ios::beg);
-    const bgfx::Memory* mem = bgfx::alloc(uint32_t(size + 1));
-    if(file.read((char*)mem->data, size)) {
-        mem->data[mem->size - 1] = '\0';
-        return mem;
+static void imguiInit() {
+    unsigned char* data;
+    int            width, height;
+    ImGuiIO&       io = ImGui::GetIO();
+
+    // Setup vertex declaration
+    imguiVertexDecl.begin()
+            .add(bgfx::Attrib::Position, 2, bgfx::AttribType::Float)
+            .add(bgfx::Attrib::TexCoord0, 2, bgfx::AttribType::Float)
+            .add(bgfx::Attrib::Color0, 4, bgfx::AttribType::Uint8, true)
+            .end();
+
+    // Create font
+    io.Fonts->AddFontDefault();
+    io.Fonts->GetTexDataAsRGBA32(&data, &width, &height);
+    imguiFontTexture = bgfx::createTexture2D((uint16_t)width, (uint16_t)height, false, 1,
+                                             bgfx::TextureFormat::BGRA8, 0,
+                                             bgfx::copy(data, width * height * 4));
+    imguiFontUniform = bgfx::createUniform("s_tex", bgfx::UniformType::Int1);
+
+    // Create shader program
+    imguiProgram =
+            loadProgram("shaders/glsl/vs_ocornut_imgui.bin", "shaders/glsl/fs_ocornut_imgui.bin");
+
+    // Setup render callback
+    io.RenderDrawListsFn = imguiRender;
+
+    // Key mapping
+    io.KeyMap[ImGuiKey_Tab]        = GLFW_KEY_TAB;
+    io.KeyMap[ImGuiKey_LeftArrow]  = GLFW_KEY_LEFT;
+    io.KeyMap[ImGuiKey_RightArrow] = GLFW_KEY_RIGHT;
+    io.KeyMap[ImGuiKey_UpArrow]    = GLFW_KEY_UP;
+    io.KeyMap[ImGuiKey_DownArrow]  = GLFW_KEY_DOWN;
+    io.KeyMap[ImGuiKey_PageUp]     = GLFW_KEY_PAGE_UP;
+    io.KeyMap[ImGuiKey_PageDown]   = GLFW_KEY_PAGE_DOWN;
+    io.KeyMap[ImGuiKey_Home]       = GLFW_KEY_HOME;
+    io.KeyMap[ImGuiKey_End]        = GLFW_KEY_END;
+    io.KeyMap[ImGuiKey_Delete]     = GLFW_KEY_DELETE;
+    io.KeyMap[ImGuiKey_Backspace]  = GLFW_KEY_BACKSPACE;
+    io.KeyMap[ImGuiKey_Enter]      = GLFW_KEY_ENTER;
+    io.KeyMap[ImGuiKey_Escape]     = GLFW_KEY_ESCAPE;
+    io.KeyMap[ImGuiKey_A]          = GLFW_KEY_A;
+    io.KeyMap[ImGuiKey_C]          = GLFW_KEY_C;
+    io.KeyMap[ImGuiKey_V]          = GLFW_KEY_V;
+    io.KeyMap[ImGuiKey_X]          = GLFW_KEY_X;
+    io.KeyMap[ImGuiKey_Y]          = GLFW_KEY_Y;
+    io.KeyMap[ImGuiKey_Z]          = GLFW_KEY_Z;
+    io.SetClipboardTextFn          = imguiSetClipboardText;
+    io.GetClipboardTextFn          = imguiGetClipboardText;
+}
+
+static void imguiRender(ImDrawData* drawData) {
+    for(int ii = 0, num = drawData->CmdListsCount; ii < num; ++ii) {
+        bgfx::TransientVertexBuffer tvb;
+        bgfx::TransientIndexBuffer  tib;
+
+        const ImDrawList* drawList    = drawData->CmdLists[ii];
+        uint32_t          numVertices = (uint32_t)drawList->VtxBuffer.size();
+        uint32_t          numIndices  = (uint32_t)drawList->IdxBuffer.size();
+
+        if(!bgfx::getAvailTransientVertexBuffer(numVertices, imguiVertexDecl) ||
+           !bgfx::getAvailTransientIndexBuffer(numIndices)) {
+            break;
+        }
+
+        bgfx::allocTransientVertexBuffer(&tvb, numVertices, imguiVertexDecl);
+        bgfx::allocTransientIndexBuffer(&tib, numIndices);
+
+        ImDrawVert* verts = (ImDrawVert*)tvb.data;
+        memcpy(verts, drawList->VtxBuffer.begin(), numVertices * sizeof(ImDrawVert));
+
+        ImDrawIdx* indices = (ImDrawIdx*)tib.data;
+        memcpy(indices, drawList->IdxBuffer.begin(), numIndices * sizeof(ImDrawIdx));
+
+        uint32_t offset = 0;
+        for(const ImDrawCmd *cmd = drawList->CmdBuffer.begin(), *cmdEnd = drawList->CmdBuffer.end();
+            cmd != cmdEnd; ++cmd) {
+            if(cmd->UserCallback) {
+                cmd->UserCallback(drawList, cmd);
+            } else if(0 != cmd->ElemCount) {
+                uint64_t state = BGFX_STATE_RGB_WRITE | BGFX_STATE_ALPHA_WRITE | BGFX_STATE_MSAA;
+                bgfx::TextureHandle th = imguiFontTexture;
+                if(cmd->TextureId != NULL) {
+                    union
+                    {
+                        ImTextureID ptr;
+                        struct
+                        {
+                            uint16_t            flags;
+                            bgfx::TextureHandle handle;
+                        } s;
+                    } texture = {cmd->TextureId};
+                    state |= BGFX_STATE_BLEND_FUNC(BGFX_STATE_BLEND_SRC_ALPHA,
+                                                   BGFX_STATE_BLEND_INV_SRC_ALPHA);
+                    th = texture.s.handle;
+                } else {
+                    state |= BGFX_STATE_BLEND_FUNC(BGFX_STATE_BLEND_SRC_ALPHA,
+                                                   BGFX_STATE_BLEND_INV_SRC_ALPHA);
+                }
+                const uint16_t xx = uint16_t(bx::fmax(cmd->ClipRect.x, 0.0f));
+                const uint16_t yy = uint16_t(bx::fmax(cmd->ClipRect.y, 0.0f));
+                bgfx::setScissor(xx, yy, uint16_t(bx::fmin(cmd->ClipRect.z, 65535.0f) - xx),
+                                 uint16_t(bx::fmin(cmd->ClipRect.w, 65535.0f) - yy));
+                bgfx::setState(state);
+                bgfx::setTexture(0, imguiFontUniform, th);
+                bgfx::setVertexBuffer(0, &tvb, 0, numVertices);
+                bgfx::setIndexBuffer(&tib, offset, cmd->ElemCount);
+                bgfx::submit(0, imguiProgram);
+            }
+
+            offset += cmd->ElemCount;
+        }
     }
-    return nullptr;
 }
 
-bgfx::ShaderHandle loadShader(const char* shader) { return bgfx::createShader(loadMemory(shader)); }
-
-bgfx::ProgramHandle loadProgram(const char* vsName, const char* fsName) {
-    bgfx::ShaderHandle vs = loadShader(vsName);
-    bgfx::ShaderHandle fs = loadShader(fsName);
-    return bgfx::createProgram(vs, fs, true);
+static void imguiShutdown() {
+    bgfx::destroyUniform(imguiFontUniform);
+    bgfx::destroyTexture(imguiFontTexture);
+    bgfx::destroyProgram(imguiProgram);
+    ImGui::Shutdown();
 }
 
-// application
+static const char* imguiGetClipboardText(void* userData) {
+    return glfwGetClipboardString((GLFWwindow*)userData);
+}
+
+static void imguiSetClipboardText(void* userData, const char* text) {
+    glfwSetClipboardString((GLFWwindow*)userData, text);
+}
+
+// =================================================================================================
+// == APPLICATION ==================================================================================
+// =================================================================================================
 
 HARDLY_SCOPED_SINGLETON_IMPLEMENT(Application);
 
 void Application::mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
     Application* app = (Application*)glfwGetWindowUserPointer(window);
-    if(action == GLFW_PRESS && button >= 0 && button < 3) {
+    if(action == GLFW_PRESS && button >= 0 && button < 3)
         app->mMousePressed[button] = true;
-    }
 }
 
 void Application::scrollCallback(GLFWwindow* window, double xoffset, double yoffset) {
@@ -69,12 +187,12 @@ void Application::scrollCallback(GLFWwindow* window, double xoffset, double yoff
 
 void Application::keyCallback(GLFWwindow*, int key, int, int action, int mods) {
     ImGuiIO& io = ImGui::GetIO();
-    if(action == GLFW_PRESS) {
+    if(action == GLFW_PRESS)
         io.KeysDown[key] = true;
-    }
-    if(action == GLFW_RELEASE) {
+
+    if(action == GLFW_RELEASE)
         io.KeysDown[key] = false;
-    }
+
     io.KeyCtrl  = io.KeysDown[GLFW_KEY_LEFT_CONTROL] || io.KeysDown[GLFW_KEY_RIGHT_CONTROL];
     io.KeyShift = io.KeysDown[GLFW_KEY_LEFT_SHIFT] || io.KeysDown[GLFW_KEY_RIGHT_SHIFT];
     io.KeyAlt   = io.KeysDown[GLFW_KEY_LEFT_ALT] || io.KeysDown[GLFW_KEY_RIGHT_ALT];
@@ -83,9 +201,8 @@ void Application::keyCallback(GLFWwindow*, int key, int, int action, int mods) {
 
 void Application::charCallback(GLFWwindow*, unsigned int c) {
     ImGuiIO& io = ImGui::GetIO();
-    if(c > 0 && c < 0x10000) {
+    if(c > 0 && c < 0x10000)
         io.AddInputCharacter((unsigned short)c);
-    }
 }
 
 void Application::cursorPosCallback(GLFWwindow*, double, double) {}
@@ -101,9 +218,9 @@ void Application::imguiEvents(float dt) {
     io.DisplayFramebufferScale =
             ImVec2(w > 0 ? ((float)displayW / w) : 0, h > 0 ? ((float)displayH / h) : 0);
     //if(glfwGetWindowAttrib(mWindow, GLFW_FOCUSED)) {
-        double mouse_x, mouse_y;
-        glfwGetCursorPos(mWindow, &mouse_x, &mouse_y);
-        io.MousePos = ImVec2((float)mouse_x, (float)mouse_y);
+    double mouse_x, mouse_y;
+    glfwGetCursorPos(mWindow, &mouse_x, &mouse_y);
+    io.MousePos = ImVec2((float)mouse_x, (float)mouse_y);
     //} else {
     //    io.MousePos = ImVec2(-1, -1);
     //}
@@ -122,8 +239,7 @@ void Application::imguiEvents(float dt) {
 }
 
 int Application::run(int argc, char** argv) {
-
-    #ifndef EMSCRIPTEN
+#ifndef EMSCRIPTEN
 // set cwd to data folder
 #ifdef _WIN32
     SetCurrentDirectory((Utils::getPathToExe() + "../../../data").c_str());
@@ -135,16 +251,13 @@ int Application::run(int argc, char** argv) {
 #endif // EMSCRIPTEN
 
     doctest::Context context(argc, argv);
-    int              res = context.run();
-
+    int              tests_res = context.run();
     if(context.shouldExit())
-        return res;
-
+        return tests_res;
 
     // Initialize the glfw
-    if(!glfwInit()) {
+    if(!glfwInit())
         return -1;
-    }
 
     // Create a window
     mWindow = glfwCreateWindow(getWidth(), getHeight(), "game", nullptr, nullptr);
@@ -166,10 +279,10 @@ int Application::run(int argc, char** argv) {
     memset(&platformData, 0, sizeof(platformData));
 #ifdef _WIN32
     platformData.nwh = glfwGetWin32Window(mWindow);
-#endif
+#endif // _WIN32
 #ifdef __APPLE__
     platformData.nwh = glfwGetCocoaWindow(mWindow);
-#endif
+#endif // __APPLE__
     bgfx::setPlatformData(platformData);
 
     bgfx::init(bgfx::RendererType::OpenGL);
@@ -194,9 +307,8 @@ int Application::run(int argc, char** argv) {
     imguiShutdown();
     bgfx::shutdown();
     glfwTerminate();
-    return ret;
+    return tests_res + ret;
 }
-
 
 bgfx::ProgramHandle      mProgram;
 bgfx::VertexBufferHandle mVbh;
@@ -331,3 +443,23 @@ void PosColorVertex::init() {
 };
 
 bgfx::VertexDecl PosColorVertex::ms_decl;
+
+const bgfx::Memory* loadMemory(const char* filename) {
+    std::ifstream   file(filename, std::ios::binary | std::ios::ate);
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+    const bgfx::Memory* mem = bgfx::alloc(uint32_t(size + 1));
+    if(file.read((char*)mem->data, size)) {
+        mem->data[mem->size - 1] = '\0';
+        return mem;
+    }
+    return nullptr;
+}
+
+bgfx::ShaderHandle loadShader(const char* shader) { return bgfx::createShader(loadMemory(shader)); }
+
+bgfx::ProgramHandle loadProgram(const char* vsName, const char* fsName) {
+    bgfx::ShaderHandle vs = loadShader(vsName);
+    bgfx::ShaderHandle fs = loadShader(fsName);
+    return bgfx::createProgram(vs, fs, true);
+}
