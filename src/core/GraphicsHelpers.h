483c074c4d0f5d319faa2f3e969954060f23b8e4
onqtam <vik.kirilov@gmail.com>
2017-06-30 15:06:46 +0300

    fixed crash... should have treated a pointer as a double pointer

--- src/core/GraphicsHelpers.h
+++ src/core/GraphicsHelpers.h
@@ -7,22 +7,22 @@ HAPI bgfx::ShaderHandle loadShader(const char* shader);
 HAPI bgfx::ProgramHandle loadProgram(const char* vsName, const char* fsName);
 
 HAPI bgfx::TextureHandle loadTexture(const char* _name, uint32 _flags = BGFX_TEXTURE_NONE,
-                                uint8_t _skip = 0, bgfx::TextureInfo* _info = nullptr);
+                                     uint8_t _skip = 0, bgfx::TextureInfo* _info = nullptr);
 
 struct Mesh;
 
 HAPI Mesh* meshLoad(const char* _filePath);
 HAPI void meshUnload(Mesh* _mesh);
-HAPI void meshSubmit(const Mesh* _mesh, uint8_t _id, bgfx::ProgramHandle _program, const float* _mtx,
-                uint64_t _state = BGFX_STATE_MASK);
+HAPI void meshSubmit(const Mesh* _mesh, uint8_t _id, bgfx::ProgramHandle _program,
+                     const float* _mtx, uint64_t _state = BGFX_STATE_MASK);
 
 // =============== GRAPHICAL RESOURCE MANAGERS =================
 
 struct ProgramHandleCreator
 {
     void create(void* storage, const std::string& name) {
-        new(storage) bgfx::ProgramHandle(
-                loadProgram((name + "_vs").c_str(), (name + "_fs").c_str()));
+        new(storage)
+                bgfx::ProgramHandle(loadProgram((name + "_vs").c_str(), (name + "_fs").c_str()));
     }
     void destroy(void* storage) {
         bgfx::destroyProgram(*static_cast<bgfx::ProgramHandle*>(storage));
@@ -30,7 +30,7 @@ struct ProgramHandleCreator
 };
 
 template class ResourceManager<bgfx::ProgramHandle, ProgramHandleCreator>;
-typedef ResourceManager<bgfx::ProgramHandle, ProgramHandleCreator> ShaderMan;
+typedef ResourceManager<bgfx::ProgramHandle, ProgramHandleCreator>         ShaderMan;
 typedef ResourceManager<bgfx::ProgramHandle, ProgramHandleCreator>::Handle ShaderHandle;
 
 struct TextureCreator
@@ -44,7 +44,7 @@ struct TextureCreator
 };
 
 template class ResourceManager<bgfx::TextureHandle, TextureCreator>;
-typedef ResourceManager<bgfx::TextureHandle, TextureCreator> TextureMan;
+typedef ResourceManager<bgfx::TextureHandle, TextureCreator>         TextureMan;
 typedef ResourceManager<bgfx::TextureHandle, TextureCreator>::Handle TextureHandle;
 
 struct MeshCreator
@@ -52,9 +52,9 @@ struct MeshCreator
     void create(void* storage, const std::string& name) {
         new(storage) Mesh*(meshLoad(name.c_str()));
     }
-    void destroy(void* storage) { meshUnload(static_cast<Mesh*>(storage)); }
+    void destroy(void* storage) { meshUnload(*static_cast<Mesh**>(storage)); }
 };
 
 template class ResourceManager<Mesh*, MeshCreator>;
-typedef ResourceManager<Mesh*, MeshCreator> MeshMan;
+typedef ResourceManager<Mesh*, MeshCreator>         MeshMan;
 typedef ResourceManager<Mesh*, MeshCreator>::Handle MeshHandle;
