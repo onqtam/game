8e470d36d71285fc8c078208aa1318a94da82645
onqtam <vik.kirilov@gmail.com>
2017-07-14 18:01:23 +0300

    forgot this...

--- src/core/ResourceManager_tests.cpp
+++ src/core/ResourceManager_tests.cpp
@@ -60,8 +60,7 @@ template class ResourceManager<managed_int, ManagedIntCreator>;
 typedef ResourceManager<managed_int, ManagedIntCreator>         ManagedIntMan;
 typedef ResourceManager<managed_int, ManagedIntCreator>::Handle ManagedIntHandle;
 
-template <>
-HA_SINGLETON_INSTANCE(ManagedIntMan);
+HA_SINGLETON_TEMPLATE_INSTANCE(ManagedIntMan);
 
 test_case("[core] testing ResourceManager") {
     ManagedIntMan man;
