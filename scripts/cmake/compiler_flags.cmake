e15b2c4061f5e71395740fa72560b63810003e84
onqtam <vik.kirilov@gmail.com>
2017-11-28 21:39:19 +0200

    no longer trying stupid hacks... the clang5 builds on the CI will fail for now because of libc++/libstdc++ problems with <optional>/<unordered_map>... wtf

--- scripts/cmake/compiler_flags.cmake
+++ scripts/cmake/compiler_flags.cmake
@@ -111,12 +111,7 @@ if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
 endif()    
 
 if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    if(NOT TOOLCHAIN STREQUAL "js")
-        ucm_add_flags(-stdlib=libc++)
-        # this hack is for build problems - as seen here: https://stackoverflow.com/questions/46580808
-        include_directories(BEFORE "/usr/include/c++/5")
-        include_directories(AFTER "/usr/include/x86_64-linux-gnu/c++/5/")
-    endif()
+    ucm_add_flags(-stdlib=libc++)
     
     list(APPEND ha_compiler_flags
         -Weverything
